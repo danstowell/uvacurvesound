70e884ac7b57b8ec0a8fe0071cf65f9767e7c20e
Dan Stowell <danstowell@users.sourceforge.net>
2014-01-11 15:30:13 +0000

    customise the two runme scripts

--- runme_selfcontrolled.bash
+++ runme_selfcontrolled.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/Applications/SuperCollider365/SuperCollider.app/Contents/Resources/sclang scrunall.scd
+/Applications/SuperCollider365/SuperCollider.app/Contents/Resources/sclang scrunall_selfcontrolled.scd
 
 
 
