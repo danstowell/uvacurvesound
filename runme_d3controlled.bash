e07d4f95b8ae52c991394c81b0a4580bb555070a
Dan Stowell <danstowell@users.sourceforge.net>
2014-02-07 17:25:24 +0000

    improvements for self running

--- runme_d3controlled.bash
+++ runme_d3controlled.bash
@@ -3,6 +3,24 @@
 killall sclang
 killall scsynth
 
+
+
+echo ""
+echo ""
+echo "*************************************"
+echo "           UVA - Momentum"
+echo ""
+echo "      sound system initialising"
+echo ""
+echo "       it takes about a minute"
+echo ""
+echo " SuperCollider system by Dan Stowell"
+echo "*************************************"
+echo ""
+echo ""
+
+sleep 20
+
 /Applications/SuperCollider365/SuperCollider.app/Contents/Resources/sclang scrunall_d3controlled.scd
 
 
